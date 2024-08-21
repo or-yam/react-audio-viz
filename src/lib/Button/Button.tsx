95242af543dd8b7139f2f9a57013d0e3adbfa1e2
Oryam <48219965+or-yam@users.noreply.github.com>
2024-08-21 17:45:27 +0300

    Remove unused components

--- src/lib/Button/Button.tsx
+++
@@ -1,5 +0,0 @@
-import { ComponentProps } from 'react';
-
-export function Button(props: ComponentProps<'button'>) {
-  return <button {...props} />;
-}
--- src/lib/Input/Input.tsx
+++
@@ -1,5 +0,0 @@
-import { ComponentProps } from 'react';
-
-export function Input(props: ComponentProps<'input'>) {
-  return <input {...props} />;
-}
