62dffd2a1f168d56cde4c74fe87b30e0478e634a
Oryam <48219965+or-yam@users.noreply.github.com>
2024-09-04 21:17:39 +0300

    Add support for server component (use client directive)

--- src/lib/AudioVisualizer/audioVisualizer.tsx
+++ src/lib/AudioVisualizer/audioVisualizer.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useCallback, useEffect, useRef } from 'react';
 import { initAudio } from '../../utils/initAudio';
 import { renderFrequencyGraph } from '../../utils/frequencyGraph';
--- src/lib/MicrophoneVisualizer/micInputVisualizer.tsx
+++ src/lib/MicrophoneVisualizer/micInputVisualizer.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { initMicrophone } from '../../utils/initMicrophone';
 import { renderFrequencyGraph } from '../../utils/frequencyGraph';
